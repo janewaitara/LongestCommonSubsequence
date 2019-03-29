4af4d504dc60714e795442d96630d9f88501f46b
janewaitara <janewaitara99@gmail.com>
2019-03-29 17:05:48 +0300

    rearranged and reformatted LongestCommonSubsequence.java

--- src/LongestCommonSubsequence.java
+++ src/LongestCommonSubsequence.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LongestCommonSubsequence {
 
 
-    public static void main(String [] args ){
+    public static void main(String[] args) {
 
         Scanner Longest = new Scanner(System.in);
 
@@ -17,25 +17,25 @@ public class LongestCommonSubsequence {
     }
 
 
-    public static void lcsMethod(String A, String B){
+    public static void lcsMethod(String A, String B) {
         int a = A.length();
         int b = B.length();
 
-        int[][] lcsArray = new int [a+1][b+1];
+        int[][] lcsArray = new int[a + 1][b + 1];
 
         //returns the length of the lcs for String A nd String B
 
         for (int i = 0; i <= a; i++) {
 
-            for (int j = 0; j <= b ; j++) {
+            for (int j = 0; j <= b; j++) {
 
                 if (i == 0 || j == 0) {
                     lcsArray[i][j] = 0;
 
-                } else if (A.charAt(i-1) == B.charAt(j-1)) {
-                    lcsArray[i][j] = lcsArray[i-1][j-1]+1;
-                } else{
-                    lcsArray[i][j] = Math.max(lcsArray[i-1][j], lcsArray[i][j-1]);
+                } else if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    lcsArray[i][j] = lcsArray[i - 1][j - 1] + 1;
+                } else {
+                    lcsArray[i][j] = Math.max(lcsArray[i - 1][j], lcsArray[i][j - 1]);
                 }
             }
 
@@ -46,29 +46,28 @@ public class LongestCommonSubsequence {
         int newIndex = index;
 
         //creating char array and store the lsc String
-        char[] lcsChar = new char[index + 1 ];
+        char[] lcsChar = new char[index + 1];
         //lcsChar [index] = '';//set the terminating character
 
-        while (a > 0 && b > 0){
+        while (a > 0 && b > 0) {
             //if curent char in A[] and B[] is the same, then current character is part of the lcs
-            if (A.charAt(a-1) == B.charAt(b-1)) {
+            if (A.charAt(a - 1) == B.charAt(b - 1)) {
                 //put current char in result
-                lcsChar[index-1] = A.charAt(a-1);
+                lcsChar[index - 1] = A.charAt(a - 1);
 
                 a--;
                 b--;
                 index--;
             }
             //if not the same , then find the larger of the two and move in the direction  of the larger
-            else if (lcsArray[a-1][b] > lcsArray[a][b-1]) {
+            else if (lcsArray[a - 1][b] > lcsArray[a][b - 1]) {
                 a--;
-            }
-            else{
+            } else {
                 b--;
             }
         }
 
-        System.out.print("LCS of " + A +" and " + B + " is ");
+        System.out.print("LCS of " + A + " and " + B + " is ");
 
         for (int i = 0; i < newIndex; i++) {
             System.out.print(lcsChar[i]);
